f6366b8d9b5f5c99b13b795c399db31991877a97
kamiladzimira <dzimiraka@gmail.com>
2022-06-10 18:06:19 +0200

    Initial commit

---
+++ TicTacToe.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "GameManager.h"
+using namespace std;
+
+int main()
+{
+    GameManager gameManager;
+    gameManager.Start ();
+    system ("pause > 0");
+    return 0;
+}
