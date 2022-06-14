b70ca84eaf22bb8aa4720403a37f9a4cff85d218
kamiladzimira <dzimiraka@gmail.com>
2022-06-14 19:35:46 +0200

    Add function playerChooices

--- Game.cpp
+++ Game.cpp
@@ -40,7 +40,7 @@ Player* Game::getPlayerFromUser (string player)
 
 void Game::displayGreeting (Player player)
 {
-	cout << "Hi " << player.getName() << ", you play with sign: " << player.getSign() << "." << endl;
+	cout << "Hi " << player.getName () << ", you play with sign: " << player.getSign () << "." << endl;
 }
 
 void Game::startGame ()
@@ -49,18 +49,17 @@ void Game::startGame ()
 
 	cout << endl;
 
-	/*playerOne = getPlayerFromUser ("one");
+	playerOne = getPlayerFromUser ("one");
 	playerTwo = getPlayerFromUser ("two");
 	system ("cls");
 
 	displayGreeting (*playerOne);
-	displayGreeting (*playerTwo);*/
-
+	displayGreeting (*playerTwo);
 	cout << endl;
 	cout << "Let's start the game!" << endl;
 	cout << endl << endl;
-	
-	displayBoard ();
+	playerChooices ();
+
 }
 
 
@@ -70,20 +69,43 @@ void Game::displayBoard ()
 
 	cout << endl; cout << endl;
 	string s = board[0][0];
-	cout << "     |     |     " << endl;
-	cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << endl;
+	cout << "\t     |     |     " << endl;
+	cout << "\t  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << endl;
 
-	cout << "_____|_____|_____" << endl;
-	cout << "     |     |     " << endl;
+	cout << "\t_____|_____|_____" << endl;
+	cout << "\t     |     |     " << endl;
 
-	cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << endl;
+	cout << "\t  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << endl;
 
-	cout << "_____|_____|_____" << endl;
-	cout << "     |     |     " << endl;
+	cout << "\t_____|_____|_____" << endl;
+	cout << "\t     |     |     " << endl;
 
-	cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
+	cout << "\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
 
-	cout << "     |     |     " << endl
-		<< endl;
+	cout << "\t     |     |     \n" << endl << endl;
 }
+
+void Game::playerChooices ()
+{
+	int counter = 0;
+	string sign;
+
+	while (counter < 5)
+	{
+		displayBoard ();
+		if (counter % 2 == 0)
+		{
+			cout << playerOne->getName () << "'s choice: " << endl;
+		}
+		else
+		{
+			cout << playerTwo->getName () << "'s choice: " << endl;
+		}
+		counter++;
+		cin >> sign;
+		system ("cls");
+	}
+
+}
+
 
